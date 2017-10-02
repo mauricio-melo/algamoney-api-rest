0ff9907a11ef0d04e527f9b848a4de5a0cef344b
mauricio-melo <mauriciomelo.contato@outlook.com>
2017-10-02 02:38:18 -0300

    primeiro commit

---
+++ src/main/java/com/algaworks/algamoney/api/config/property/AlgamoneyApiProperty.java
@@ -0,0 +1,37 @@
+package com.algaworks.algamoney.api.config.property;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("algamoney")
+public class AlgamoneyApiProperty {
+
+    private String origemPermitida = "http://localhost:8000";
+    public final Seguranca seguranca = new Seguranca();
+
+    public String getOrigemPermitida() {
+        return origemPermitida;
+    }
+
+    public void setOrigemPermitida(String origemPermitida) {
+        this.origemPermitida = origemPermitida;
+    }
+
+    public Seguranca getSeguranca() {
+        return seguranca;
+    }
+
+    public static class Seguranca{
+
+        public boolean isEnableHttps() {
+            return enableHttps;
+        }
+
+        public void setEnableHttps(boolean enableHttps) {
+            this.enableHttps = enableHttps;
+        }
+
+        private boolean enableHttps;
+
+    }
+}
+
