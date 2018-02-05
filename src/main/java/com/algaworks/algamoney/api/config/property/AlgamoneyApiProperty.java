603d1e0386a750911aa3fa6933bb942708912d9a
mauricio-melo <mauriciomelo.contato@outlook.com>
2018-02-05 18:18:31 -0200

    Alteração de origin-permitida para a do frontend

--- src/main/java/com/algaworks/algamoney/api/config/property/AlgamoneyApiProperty.java
+++ src/main/java/com/algaworks/algamoney/api/config/property/AlgamoneyApiProperty.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("algamoney")
 public class AlgamoneyApiProperty {
 
-    private String origemPermitida = "http://localhost:4200";
+    private String origemPermitida = "https://algamoney-ui-mm.herokuapp.com";
     public final Seguranca seguranca = new Seguranca();
 
     public String getOrigemPermitida() {
