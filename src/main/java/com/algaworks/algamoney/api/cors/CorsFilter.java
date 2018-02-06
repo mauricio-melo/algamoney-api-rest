ac98959ecc6cb5425df95856102fa7d3589e21e0
mauricio-melo <mauriciomelo.contato@outlook.com>
2018-02-05 23:05:33 -0200

    Ajuste CORS

--- src/main/java/com/algaworks/algamoney/api/cors/CorsFilter.java
+++ src/main/java/com/algaworks/algamoney/api/cors/CorsFilter.java
@@ -17,6 +17,8 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+
+
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
@@ -31,7 +33,7 @@ public class CorsFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) resp;
 
-        response.setHeader("Access-Control-Allow-Origin", algamoneyApiProperty.getOriginPermitida();
+        response.setHeader("Access-Control-Allow-Origin", algamoneyApiProperty.getOriginPermitida());
         response.setHeader("Access-Control-Allow-Credentials", "true");
 
         if ("OPTIONS".equals(request.getMethod()) && algamoneyApiProperty.getOriginPermitida().equals(request.getHeader("Origin"))) {
